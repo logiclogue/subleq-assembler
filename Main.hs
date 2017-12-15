f5d5a63ecd9736f2d8580c32bf1659fd1a993648
logiclogue <jordanpaullord@hotmail.co.uk>
2017-12-15 10:21:45 +0000

    Adding wordToByte, failing

--- Main.hs
+++ Main.hs
@@ -1,9 +1,17 @@
 module Main where
 
 import Data.Word
+import Data.Bits
 
 data Instruction = Subleq Word16 Word16 Word16
     deriving (Show)
+
+wordToByte :: Word16 -> (Word8, Word8)
+wordToByte x = (left x, right x) where
+    left :: Word16 -> Word8
+    left x  = (x (.&.) 0xFF00) `shiftR` 8
+    right :: Word16 -> Word8
+    right x = x (.&.) 0x00FF
 
 main :: IO ()
 main = do
