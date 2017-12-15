00145095677f20fc8a4c8a8c0ac31f9a45ec7fda
logiclogue <jordanpaullord@hotmail.co.uk>
2017-12-15 10:15:18 +0000

    Adds instruction

--- Main.hs
+++ Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+import Data.Word
+
+data Instruction = Subleq Word16 Word16 Word16
+    deriving (Show)
+
 main :: IO ()
 main = do
-    putStrLn "Testing"
+    putStrLn $ show $ Subleq 10 10 10
